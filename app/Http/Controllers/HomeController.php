543c088e219015804be430c51f5ffef4740355b9
do.van.huy <huydo20071999@gmail.com>
2021-08-27 13:07:31 +0700

    function show lesson list

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Course;
+use App\Models\Lesson;
 
 class HomeController extends Controller
 {
@@ -12,5 +13,13 @@ class HomeController extends Controller
         $courses=Course::with('image')->orderBy('created_at')->get();
 
         return view('pages.user.courses_list', compact('courses'));
+    }
+
+    public function showLesson($id)
+    {
+        $lessons=Lesson::orderBy('id', 'asc')->where('course_id', $id)->get();
+        $course=Course::where('id', $id)->first();
+        
+        return view('pages.user.lesson_list', compact('lessons', 'course'));
     }
 }
